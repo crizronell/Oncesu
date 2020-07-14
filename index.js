b51a85a5af51c6cc813c2e004d2153d15d434f9e
crizronell <rvillamil_180000000794@uic.edu.ph>
2020-07-14 21:17:41 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
+const navSlide = () => {
+    const hamburger = document.querySelector('.hamburger');
+    const nav  = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
-navBarToggle.addEventListener('click', function () {
+    //toggle Nav
+    hamburger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
     
-    mainNav.classList.toggle('active');
-});
+
+
+        navLinks.forEach((link, index) => {
+			if(link.style.animation){
+				link.style.animation = '';
+			}else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                console.log(index / 7 );
+			}
+		});
+
+	hamburger.classList.toggle('toggle');
+	});
+      
+}
+
+navSlide();
